041552a365e841aadb95fb1d300c7e6782ade75f
Gustavo Pasquini <gustavopasquini2006@hotmail.com>
2026-03-18 20:16:29 -0300

    Adicionando controller de Livro

---
+++ src/main/java/unicesumar/gerenciamento_de_livros/controllers/LivroController.java
@@ -0,0 +1,46 @@
+package unicesumar.gerenciamento_de_livros.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+import unicesumar.gerenciamento_de_livros.models.LivroModel;
+import unicesumar.gerenciamento_de_livros.services.LivroService;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/livros")
+public class LivroController {
+
+    @Autowired
+    private LivroService livroService;
+
+    @PostMapping
+    public ResponseEntity<LivroModel> criarLivro(@RequestBody LivroModel livroModel) {
+        LivroModel request = livroService.criarLivro(livroModel);
+        URI uri =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(request.getId()).toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LivroModel>> listarLivros() {
+        List<LivroModel> request = livroService.listarLivros();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping(path="/{id}")
+    public ResponseEntity<LivroModel> buscarLivroPorId(@PathVariable Long id) {
+        LivroModel request = livroService.buscarLivroPorId(id);
+        return ResponseEntity.ok().body(request);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deletarLivroPorId(@PathVariable Long id) {
+        livroService.deletarLivroPorId(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
