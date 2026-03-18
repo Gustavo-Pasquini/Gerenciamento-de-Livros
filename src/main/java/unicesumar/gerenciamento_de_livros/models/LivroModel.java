73338c838fdd04f7d669319179c391d3ef58a78e
Gustavo Pasquini <gustavopasquini2006@hotmail.com>
2026-03-18 19:41:05 -0300

    Adicionando model de Livro

---
+++ src/main/java/unicesumar/gerenciamento_de_livros/models/LivroModel.java
@@ -0,0 +1,54 @@
+package unicesumar.gerenciamento_de_livros.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tb_livro")
+public class LivroModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="tituloLivro")
+    private String titulo;
+    @Column(name="autorLivro")
+    private String autor;
+    @Column(name="anoPublicacaoLivro")
+    private int anoPublicacao;
+
+    public LivroModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+}
